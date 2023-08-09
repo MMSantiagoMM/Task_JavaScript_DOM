045bee818cc5c37eaaebe3e9d509ecdfb44d922c
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-08-09 16:37:14 -0500

    ejercicio 5 resuelto

---
+++ 5/index.js
@@ -0,0 +1,12 @@
+let Texto = document.querySelector('#text')
+
+
+
+function myFunction(){
+    if(Texto.style.display =='none'){
+        Texto.style.display='block'
+    }else{
+        Texto.style.display='none'
+    }
+    
+}
