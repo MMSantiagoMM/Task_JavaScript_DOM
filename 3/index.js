c154a81d283dd93a12875f55cdb18b517ac0a7dd
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-08-09 16:11:58 -0500

    punto tres solucionado

---
+++ 3/index.js
@@ -0,0 +1,18 @@
+let Button = document.querySelector('.openbtn');
+let SideBar = document.getElementById('mySidebar')
+let Links = document.getElementsByTagName('a');
+let Main = document.getElementById('main')
+let ButtonClose = document.querySelector('.closebtn')
+
+
+
+Button.addEventListener('click',()=>{
+   SideBar.style.left = '0px'
+    Main.style.marginLeft = '200px'
+})
+
+ButtonClose.addEventListener('click',()=>{
+    SideBar.style.left = '-200px'
+    Main.style.marginLeft = '0px'
+})
+
